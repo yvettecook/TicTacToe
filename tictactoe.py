fc78954c1a01194fb7b358979d59712450440c08
Yvette <yvettecook@gmail.com>
2014-12-26 15:29:45 +0000

    Added player - new class

--- tictactoe.py
+++ tictactoe.py
@@ -1,6 +1,11 @@
 class Tictactoe(object):
     def __init__(self):
         self.board = [[None, None, None],[None, None, None],[None, None, None]]
+        self.player = Player
 
     def board():
         return self.board
+
+
+class Player(object):
+    pass
--- tictactoe_tests.py
+++ tictactoe_tests.py
@@ -14,6 +14,11 @@ def test_row_has_3_squares():
     assert len(game.board[0]) == 3
     print "passed: board has 3 squares"
 
+def test_game_has_player():
+    assert game.player != None
+    print "passed: game has a player"
+
 test_game_has_board()
 test_board_has_3_rows()
 test_row_has_3_squares()
+test_game_has_player()
